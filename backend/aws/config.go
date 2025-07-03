762b2a19cf941ebc4fe28b94faf60e8f69722dab
Mudit Maheshwari <140621191+mudit06mah@users.noreply.github.com>
2025-07-03 14:55:01 +0530

    initial commit

---
+++ backend/aws/config.go
@@ -0,0 +1,14 @@
+package aws
+
+import (
+  "context"
+  "log"
+  "github.com/aws/aws-sdk-go-v2/config"
+)
+
+// ...
+
+cfg, err := config.LoadDefaultConfig(context.TODO())
+if err != nil {
+  log.Fatalf("failed to load configuration, %v", err)
+}
